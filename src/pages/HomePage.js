1b1d8afb6bc5b0f3d5499eacb54dce33aa0ad7bb
FFAI888 <453870150@qq.com>
2025-09-20 09:10:21 +0800

    Create HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { WalletContext } from "../context/WalletContext";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  const { walletAddress, isVerified } = useContext(WalletContext);
+
+  useEffect(() => {
+    if (!walletAddress || !isVerified) {
+      navigate("/");
+    }
+  }, [walletAddress, isVerified, navigate]);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>首页</h1>
+      {walletAddress && <p>已登录钱包: {walletAddress}</p>}
+      <p>欢迎来到你的Dapp首页！</p>
+    </div>
+  );
+};
+
+export default HomePage;

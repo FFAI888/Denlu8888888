230130aae4eeae9780d1a6cb144d064df198f349
FFAI888 <453870150@qq.com>
2025-09-20 09:08:37 +0800

    Update and rename style.css to src/App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import ConfirmPage from "./pages/ConfirmPage";
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/confirm" element={<ConfirmPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
